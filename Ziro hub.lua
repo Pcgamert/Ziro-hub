ddff669a783586f156eadd1e28d36f7e3afff278
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2023-04-15 16:38:40 +0300

    Update Ziro hub.lua

--- Ziro hub.lua
+++ Ziro hub.lua
@@ -39,7 +39,7 @@ Section:NewButton("Red", "", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(484.459839, -9.69246006, -75.7483749, -0.977502048, -1.69424688e-08, 0.210925981, -8.24833979e-09, 1, 4.20986552e-08, -0.210925981, 3.94117308e-08, -0.977502048)
 end)
 
-Selection:NewButton("Blue", "", function()
+Section:NewButton("Blue", "", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(484.498322, -9.69246006, 289.558777, -0.95287931, 3.57582373e-08, 0.303349674, 2.60836708e-09, 1, -1.09684564e-07, -0.303349674, -1.03724901e-07, -0.95287931)
 end)
 
