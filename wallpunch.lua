f3c4d74c6866afdbf007f2acbc12c77f23ebb87d
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2023-05-31 00:05:44 +0300

    Update wallpunch.lua

--- wallpunch.lua
+++ wallpunch.lua
@@ -1,1 +1,4 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pcgamert/build-a-boat-for-treashure-script/main/Gui.lua"))()
+
+local Window = Library.CreateLib("Ziro hub", "RJTheme3")
 
