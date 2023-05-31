83076a842cb42d2b860ea90446d6a1bde593db00
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2023-05-31 11:27:12 +0300

    Update wallpunch.lua

--- wallpunch.lua
+++ wallpunch.lua
@@ -5,6 +5,6 @@ local Window = Library.CreateLib("Ziro hub", "RJTheme3")
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("Main")
 
-Section:NewDropdown("DropdownText", "DropdownInf", {"Option 1", "Option 2", "Option 3"}, function(currentOption)
-    print(currentOption)
+Section:NewDropdown("DropdownText", "DropdownInf", {"Arena 1", "Option 2", "Option 3"}, function(option)
+    if option == Arena 1 then print("hello world")
 end)
