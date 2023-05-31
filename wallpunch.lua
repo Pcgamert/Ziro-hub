140e191fb1a05decde2482eb05526a1249131565
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2023-05-31 11:56:40 +0300

    Update wallpunch.lua

--- wallpunch.lua
+++ wallpunch.lua
@@ -9,6 +9,7 @@ Section:NewDropdown("DropdownText", "DropdownInf", {"Arena 1", "Arena 2", "Arena
     if Option == "Arena 1" then
     while wait() do 
    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-111.481918, 1.22658122, -29.0489521, 0.00292778015, 0, 0.999995708, 0, 1, 0, -0.999995708, 0, 0.00292778015)
+wait(1)
 end
 end)
 
