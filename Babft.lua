c26b4a77e89a4e87e630abb0ab1179b172f174d2
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2023-06-21 16:45:11 +0300

    Update Babft.lua

--- Babft.lua
+++ Babft.lua
@@ -16,7 +16,7 @@ tab:CreateButton("Black", "ButtonInfo", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.BlackTeam.Spawns.SpawnLocation.CFrame
 end)
 
-tab:CreateButton("White", "ButtonInfo", function()
+tab2:CreateButton("White", "ButtonInfo", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.WhiteTeam.Spawns.SpawnLocation.CFrame
 end)
 
@@ -24,17 +24,17 @@ tab:CreateButton("Green", "", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.CamoTeam.Spawns.SpawnLocation.CFrame
 end)
 
-tab:CreateButton("Magenta", "", function()
+tab2:CreateButton("Magenta", "", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.MagentaTeam.Spawns.SpawnLocation.CFrame
 end)
 
-tab:CreateButton("Red", "", function()
+tab2:CreateButton("Red", "", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(484.459839, -9.69246006, -75.7483749, -0.977502048, -1.69424688e-08, 0.210925981, -8.24833979e-09, 1, 4.20986552e-08, -0.210925981, 3.94117308e-08, -0.977502048)
 end)
-("Blue", "", function()
+tab2:CreateButton("Blue", "", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(484.498322, -9.69246006, 289.558777, -0.95287931, 3.57582373e-08, 0.303349674, 2.60836708e-09, 1, -1.09684564e-07, -0.303349674, -1.03724901e-07, -0.95287931)
 end)
 
-tab:CreateButton("Yellow", "ButtonInfo", function()
+tab2:CreateButton("Yellow", "ButtonInfo", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-576.394958, -13.1000061, 610.19989, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 end)
