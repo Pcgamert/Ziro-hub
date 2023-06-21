9ceaaa97e9fcc6530241d5cd6f2dab2d7c003363
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2023-06-21 15:01:09 +0300

    Update Gui.lua

--- Gui.lua
+++ Gui.lua
@@ -12,7 +12,7 @@ function Kavo:DraggingEnabled(frame, parent)
     parent = parent or frame
     
     -- Тут начинается приступ говнокода 5 степени :)
-    local dragging = false
+    local dragging = true
     local dragInput, mousePos, framePos
 
     frame.InputBegan:Connect(function(input)
