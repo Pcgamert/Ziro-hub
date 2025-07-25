c05fde40220f51f7e13a2b63ea165d746b74df16
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2025-07-25 20:56:40 +0300

    Update new ui.lua

--- ui-test/new ui.lua
+++ ui-test/new ui.lua
@@ -1,33 +1,63 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Pcgamert/Ziro-hub/refs/heads/Scripts/utils.lua"))()
-
-
 local scrg = Instance.new("ScreenGui")
 scrg.Name = "ScreenGui"
 scrg.Parent = game.CoreGui
 
  local main = Instance.new("Frame")
  main.Name = "Main"
- main.Size = UDim2.new(0,650,0,450)
+ main.Size = UDim2.new(0,450,0,300)
  main.Position = UDim2.new(0.300,0,0,150)
- main.BackgroundColor3 = Color3.fromRGB(39, 40, 41)
+ main.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
  main.Parent = scrg
 
  local x = Instance.new("TextButton")
  x.Name = "Close"
  x.Size = UDim2.new(0,23,0,23)
- x.Position = UDim2.new(0,626,0,1)
+ x.Position = UDim2.new(0,426,0,1)
  x.Parent = main
  x.Text = 'X'
  x.TextColor3 = Color3.fromRGB(255,255,255)
- x.BackgroundColor3 = Color3.fromRGB(39, 40, 41)
+ x.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
  x.AutoButtonColor = false
 
  local line = Instance.new("Frame")
  line.Name = "Line"
- line.Size = UDim2.new(0,648,0,5)
+ line.Size = UDim2.new(0,448,0,5)
  line.Position = UDim2.new(0,1,0,25)
- line.BackgroundColor3 = Color3.fromRGB(53, 54, 55)
+ line.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+ line.BorderSizePixel = 0
  line.Parent = main
+
+ local scf1 = Instance.new("ScrollingFrame")
+ scf1.Name = "Tab1"
+ scf1.Parent = main
+ scf1.Size = UDim2.new(0,150,0,268)
+ scf1.Position = UDim2.new(0,1,0,31)
+ scf1.AutomaticCanvasSize = Enum.AutomaticSize.Y
+ scf1.BackgroundColor3 = Color3.fromRGB(33,33,33)
+ scf1.BorderSizePixel = 0
+ scf1.ScrollBarThickness = 8
+ scf1.ClipsDescendants = true
+ local scf2 = Instance.new("ScrollingFrame")
+ scf2.Name = "Tab2"
+ scf2.Parent = main
+ scf2.Size = UDim2.new(0,150,0,268)
+ scf2.Position = UDim2.new(0,151,0,31)
+ scf2.AutomaticCanvasSize = Enum.AutomaticSize.Y
+ scf2.BackgroundColor3 = Color3.fromRGB(33,33,33)
+ scf2.BorderSizePixel = 0
+ scf2.ScrollBarThickness = 8
+ scf2.ClipsDescendants = true
+ local scf3 = Instance.new("ScrollingFrame")
+ scf3.Name = "Tab3"
+ scf3.Parent = main
+ scf3.Size = UDim2.new(0,150,0,268)
+ scf3.Position = UDim2.new(0,300,0,31)
+ scf3.AutomaticCanvasSize = Enum.AutomaticSize.Y
+ scf3.BackgroundColor3 = Color3.fromRGB(33,33,33)
+ scf3.BorderSizePixel = 0
+ scf3.ScrollBarThickness = 8
+ scf3.ClipsDescendants = true
+
 
  local corner = Instance.new("UICorner")
  corner.CornerRadius = UDim.new(0, 15)
@@ -36,7 +66,15 @@ scrg.Parent = game.CoreGui
  local corner1 = Instance.new("UICorner")
  corner1.CornerRadius = UDim.new(0, 15)
  corner1.Parent = x
-
+ local corner2 = Instance.new("UICorner")
+ corner2.CornerRadius = UDim.new(0, 21)
+ corner2.Parent = scf1
+ local corner3 = Instance.new("UICorner")
+ corner3.CornerRadius = UDim.new(0, 21)
+ corner3.Parent = scf2
+ local corner4 = Instance.new("UICorner")
+ corner4.CornerRadius = UDim.new(0, 21)
+ corner4.Parent = scf3
 
     x.MouseButton1Click:Connect(function()
     scrg:Destroy()
