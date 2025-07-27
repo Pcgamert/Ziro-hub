23575750d981ebfbcbfb61a5f4d51f04c87a4857
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2025-07-27 04:46:04 +0300

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -203,7 +203,7 @@ function UI()
             {name = "DarkDexV4(1.1.0)", callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Pcgamert/Ziro-hub/refs/heads/Scripts/DarkDexv4(alpha1.1.0).lua"))() end},
             },
             RemoteSpy = {
-            {name = "SimpleSpy lite", callback = function() loadstring(game:HttpGet('https://raw.githubusercontent.com/Pcgamert/Ziro-hub/refs/heads/Scripts/SimpleSpy%20lite.lua'))() end},
+            {name = "SimpleSpy lite", callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Pcgamert/Ziro-hub/refs/heads/Scripts/SimpleSy%20Lite2.lua"))() end},
             },
         },
         Misc = {
