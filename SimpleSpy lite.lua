066008420b839632dd5da32f5d35e6c8f3663941
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2025-07-27 04:42:59 +0300

    Update SimpleSpy lite.lua

--- SimpleSpy lite.lua
+++ SimpleSpy lite.lua
@@ -2461,15 +2461,3 @@ print('a2')
         Frosty - GUI to Lua
         OSINT BOSS - Fixing simplespy, hooks, func, etc so it works for low level executor (also works for roblox studio environment)
 ]]
-local settings = {
-    SaveDecompileLogs = true, -- saves decompile logs so you dont have to decompile again
-    SaveScanLogs = true, -- saves scan logs (scans for localscript to decompile) so you dont have to scan again
-    ScanForNewInstance = true, -- scans for new localscript and decompile it and add it to the decompile logs
-    InterceptUntilRan = true, -- blocks request until you manually run it (i recommend when bypassing keys)
-    CursorOffset = -15, -- Cursor offset
-    PathToDump = {game.Players.LocalPlayer, game:GetService('ReplicatedStorage')} -- path to dump
-
-}
---// Init 
-_G.data = settings
-warn "Need reolad game If closed" 
