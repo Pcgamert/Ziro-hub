0e0debd16899aa044ab2107f2b9c25008723e2a4
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2025-07-29 16:39:23 +0300

    Update SimpleSpy lite.lua

--- SimpleSpy lite.lua
+++ SimpleSpy lite.lua
@@ -1,12 +1,14 @@
---[[
-    SimpleSpy is a lightweight penetration testing tool that logs remote calls.
+local settings = {
+    SaveDecompileLogs = true, -- saves decompile logs so you dont have to decompile again
+    SaveScanLogs = true, -- saves scan logs (scans for localscript to decompile) so you dont have to scan again
+    ScanForNewInstance = true, -- scans for new localscript and decompile it and add it to the decompile logs
+    InterceptUntilRan = true, -- blocks request until you manually run it (i recommend when bypassing keys)
+    CursorOffset = -15, -- Cursor offset
+    PathToDump = {game.Players.LocalPlayer, game:GetService('ReplicatedStorage')} -- path to dump
 
-    Credits:
-        exx - basically everything
-        Frosty - GUI to Lua
-        OSINT BOSS - Fixing the hooks so it works for low level executor (also works for roblox studio environment)
-]]
---// Rhook installation
+}
+--// Init 
+_G.data = settings
 local decompile = decompile or warn('ERROR LEVEL 3 : EXPLOIT FUNCTION NOT SUPPORTED (DECOMPILE)')
 local makefolder = makefolder or warn('ERROR LEVEL 3 : EXPLOIT FUNCTION NOT SUPPORTED (DECOMPILE)')
 local writefile = writefile or warn('ERROR LEVEL 3 : EXPLOIT FUNCTION NOT SUPPORTED (DECOMPILE)')
