f613429428f5f026eeac3c0e031a75b4ac70346e
Pcgamert <91797332+Pcgamert@users.noreply.github.com>
2025-10-13 22:46:11 +0300

    Update and rename 6570537965 to 6570537965.lua

--- scripts/6570537965.lua
+++ scripts/6570537965.lua
@@ -1,4 +1,6 @@
 local rns = game:GetService("RunService")
 rns.RenderStepped:Connect(function()
+if game.Players.LocalPlayer.Character.Humnaoid then
 game.Players.LocalPlayer.Character.Humanoid.Jump = true
+end
 end)
